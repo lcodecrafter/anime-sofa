1012aa86cfdb28155629d3ca898a249cb0ca6021
lCodecrafter <lcodecrafter@gmail.com>
2023-01-05 12:47:58 +0100

    Add globals styles and font.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Anime Sofa</title>
+        <meta name="description" content="See your favourite anime series without waiting." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
